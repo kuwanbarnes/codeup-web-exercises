8d77c4beb2ff81f2741957e8955bfaa03210a5a3
kuwan barnes <Kuwan.barne@gmail.com>
2020-11-14 01:16:37 -0600

    "fixed_do_while loops explained"

--- js/do_while.js
+++ js/do_while.js
@@ -3,19 +3,26 @@ while(m < 65536){
     console.log(m);
     m = m * 2;
 }
+//An ice cream seller can't go home until she sells all of her cones.
+// First write enough code that generates a random number between 50 and 100
+// representing the amount of cones to sell before you start your loop.
+// Inside of the loop your code should generate another random number between 1 and 5,
+// simulating the amount of cones being bought by her clients.
+// Use a do-while loop to log to the console the amount of cones sold to each person.
 
+// This is a way get the random numbers for this exercise.
 // This is how you get a random number between 50 and 100
 var allCones = Math.floor(Math.random() * 50) + 50;
 //This is how many cones I start the day with
 console.log("I only have this many cones to sell: " + allCones
     );
-// This expression will generate a random number between 1 and 5
-
 
 do{
+    // This expression will generate a random number between 1 and 5
+
     var conesBought = Math.floor(Math.random() * 5) + 1;
+    //not enough cones to sell
     if (conesBought >allCones){
-        //not enough cones to sell
         console.log("Sorry, try again tomorrow for "+ conesBought + "I only have : "+ allCones);
 
     }else{
@@ -26,5 +33,5 @@ do{
 }
 //condition that runs the loop
 while (allCones > 0)
-//ran out of cones
+//sold out of cones
 console.log("Yay! I sold them all!")
