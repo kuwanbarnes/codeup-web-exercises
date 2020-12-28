8981fa765c69bc4839a68b80c832abfc5a31619c
kuwan barnes <Kuwan.barne@gmail.com>
2020-12-27 21:02:52 -0600

    working on konamiCode

--- js/practice.js
+++ js/practice.js
@@ -985,3 +985,23 @@ let newVariable = 'Playing around with typeof.';
 console.log(typeof newVariable);
 newVariable = 1;
 console.log(typeof newVariable);
+let sale = true;
+sale =false;
+if (sale=== true){
+    console.log ('Time to buy!')
+}
+let sale = true;
+
+sale = false;
+
+if(sale) {
+    console.log('Time to buy!');
+}else{
+    console.log('Time to wait for a sale.')
+}
+let hungerLevel = 7;
+if(hungerLevel >7){
+    console.log('Time to eat!')
+}else{
+    console.log('We can eat later!')
+}
