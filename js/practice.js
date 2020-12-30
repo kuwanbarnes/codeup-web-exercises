20d4d75626df1bfec053bcbfb1738f660663f45a
kuwan barnes <Kuwan.barne@gmail.com>
2020-12-29 18:18:51 -0600

    switchstatements

--- js/practice.js
+++ js/practice.js
@@ -1063,7 +1063,21 @@ function firstAndLast(input) {
     } else {
         console.log('Better get to work!');
     }
-
+    let athleteFinalPosition = 'first place';
+    switch(athleteFinalPosition){
+        case 'first place':
+            console.log('You get the gold medal!');
+            break
+        case 'second place':
+            console.log('You get the silver medal!');
+            break
+        case 'third place':
+            console.log('You get the bronze medal!');
+            break
+        default:
+            console.log('No medal awarded.');
+            break
+    }
 
     let favoritePhrase = '';
 
@@ -1105,3 +1119,18 @@ function firstAndLast(input) {
     }}
     favoritePhrase1?console.log('I love that!'):console.log("I don't love that!");
 favoritePhrase === 'Love That!' ? console.log('I love that!') : console.log("I don't love that!");
+
+let season = 'summer';
+
+if (season === 'spring') {
+    console.log('It\'s spring! The trees are budding!');
+} else if(season === 'winter'){
+    console.log('It\'s winter! Everything is covered in snow.')
+}else if(season === 'fall'){
+    console.log('It\'s fall! Leaves are falling!')
+}else if (season === 'summer'){
+    console.log('It\'s sunny and warm because it\'s summer!')
+}
+else {
+    console.log('Invalid season.');
+}
